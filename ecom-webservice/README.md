de01249c9b07ce33f831fe03d40a003bd656dd25
Waheed <wahid.khan74@gmail.com>
2022-05-13 17:00:27 +0530

    added docker setup  basic command and building a custom docker image

---
+++ ecom-webservice/README.md
@@ -0,0 +1,7 @@
+# Spring boot Web service application.
+- Use for sample jenkins job execution
+- Use for automated CI / CD pipleline
+- Use for Docker image creation
+
+
+
---
+++ ecom-webservice/src/main/java/com/ecomerce/webapp/EmployeeDAO.java
@@ -0,0 +1,29 @@
+package com.ecomerce.webapp;
+
+import org.springframework.stereotype.Repository;
+ 
+import com.ecomerce.webapp.Employee;
+import com.ecomerce.webapp.Employees;
+ 
+@Repository
+public class EmployeeDAO 
+{
+    private static Employees list = new Employees();
+     
+    static
+    {
+        list.getEmployeeList().add(new Employee(1, "Alex", "Kolenchiskey", "abc@gmail.com"));
+        list.getEmployeeList().add(new Employee(2, "David", "Kameron", "titanic@gmail.com"));
+        list.getEmployeeList().add(new Employee(3, "Sam", "Kameron", "sam@gmail.com"));
+        list.getEmployeeList().add(new Employee(4, "John", "Smith", "john@gmail.com"));
+    }
+     
+    public Employees getAllEmployees() 
+    {
+        return list;
+    }
+     
+    public void addEmployee(Employee employee) {
+        list.getEmployeeList().add(employee);
+    }
+}
